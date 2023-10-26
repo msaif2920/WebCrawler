0f34caf9ffbab25f5910e729b9ffd5e710fb8b57
Mohammad Saif <saminsaif29@gmail.com>
2023-10-26 00:04:20 -0400

    crawl recursively

--- crawl.js
+++ crawl.js
@@ -1,25 +1,51 @@
 const {JSDOM} = require('jsdom');
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages;
+    }
+
+    const normalizeCurrentUrl = normalizeURL(currentURL);
+    if(pages[normalizeCurrentUrl] > 0) {
+        pages[normalizeCurrentUrl]++;
+        return pages; 
+    }
+
+    pages[normalizeCurrentUrl] = 1
     console.log(`actively crawling: ${currentURL}`);
-    try {
+
+    try { 
     const resp = await fetch(currentURL);
 
     if(resp.status > 399) {
         console.log(`error in fetch with b stats code: ${resp.status} on page ${currentURL}`);
-        returnl
+        return pages;
     }
 
     const contentType = resp.headers.get("content-type")
     if(!contentType.includes("text/html")) {
         console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`);
-        return;
+        return pages;
     }
 
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    console.log(htmlBody)
+    console.log(nextURLs)
+    
+    for(const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
     } catch(err){
         console.log(`error in fetch: ${err.message} on ${currentURL}`);
     }
+
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
